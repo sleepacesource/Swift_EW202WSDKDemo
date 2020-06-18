236f27fa01b9eba1398d97ff195d8e68b7d34ce2
Shawsan@sleepace.net <Shawsan@sleepace.net>
2020-06-18 11:13:04 +0800

    build project!

---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, '9.0'
+
+target 'Swift_EW202WSDKDemo' do
+    pod 'AFNetworking'
+end
---
+++ Swift_EW202WSDKDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift_EW202WSDKDemo
+//
+//  Created by San on 18/6/2020.
+//  Copyright © 2020 medica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
